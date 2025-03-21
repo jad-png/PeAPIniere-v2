31a0ae0c06a03817e64970fdd487d07da65d0618
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-21 11:07:01 +0000

    Add authentication routes for login, registration, logout, and profile access

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// auth routes
+Route::get('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post("/logout", [AuthController::class, 'logout'])->middleware("jwt.auth");
+Route::get("/profile", [AuthController::class, 'profile'])->middleware("jwt.auth");
