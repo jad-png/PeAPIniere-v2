aa797a9baa85d00503e745396def8d180b90c21c
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-21 11:07:22 +0000

    Add JWT middleware alias to application configuration

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -6,13 +6,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'jwt.auth' => \App\Http\Middleware\JwtMiddleware::class,
+            // 'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            // 'client' => \App\Http\Middleware\ClientMiddleware::class,
+            // 'employee' => \App\Http\Middleware\EmployeeMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
