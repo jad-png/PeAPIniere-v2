6a7f3c89f4484f2c6480c22ea6e7c98c62cef11e
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-22 23:59:13 +0000

    Implement slug functionality in Plant model for improved URL handling

--- api/app/Models/Plant.php
+++ api/app/Models/Plant.php
@@ -4,17 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Plant extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
     protected $fillable = [
         "name",
         "description",
         "price",
         "image",
-        "category_id"
+        "category_id",
+        "slug"
     ];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom("name")
+            ->saveSlugsTo("slug")
+            ->doNotGenerateSlugsOnUpdate();
+    }
 
     public function category()
     {
