ab48c74241c91f9e00d96021b122c17675436d82
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-22 22:45:36 +0000

    Add admin routes for sales data and plant statistics in api.php

--- api/routes/api.php
+++ api/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\PlantController;
+use App\Http\Controllers\StatsController;
 
 // auth routes
 Route::get('/login', [AuthController::class, 'login']);
@@ -19,3 +20,9 @@ Route::apiResource("orders", OrderController::class)->middleware("jwt.auth");
 
 Route::get("orders/{order}/plants", [OrderController::class, "plantsOrder"]);
 Route::get("orders/{order}/cancel", [OrderController::class, "cancel"]);
+
+Route::prefix("admin")->group(function () {
+    Route::get("/sales", [StatsController::class, "sales"]);
+    Route::get("/popular-plants", [StatsController::class, "popularPlants"]);
+    Route::get("/plants-repartition", [StatsController::class, "plantsRepartition"]);
+})->middleware("jwt.auth");
