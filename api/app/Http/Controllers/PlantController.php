0a6b2dd45ecf35d32cddb201780f5a6262b8e4e6
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-22 15:39:36 +0000

    Add PlantController for managing plants with CRUD operations

---
+++ api/app/Http/Controllers/PlantController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Plant;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class PlantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return $this->sendResponse("All plants.", Plant::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $fields = $request->validate([
+                "name" => ['required'],
+                "description" => ['required'],
+                "image" => ['required'],
+                "price" => ['required', 'numeric', 'min:10'],
+                "category_id" => ['required']
+            ]);
+        } catch (ValidationException $e) {
+            return $this->sendError("Validation errors", $e->errors(), 422);
+        }
+
+        $category = Category::find($fields["category_id"]);
+        if (!$category) {
+            return $this->sendError("Category ID " . $fields["category_id"] . " does not exists", [], 404);
+        }
+
+        $plant = Plant::create($fields);
+        return response()->json(['plant' => $plant], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Plant $plant)
+    {
+        return response()->json(['Plant' => $plant], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Plant $plant)
+    {
+        try {
+            $fields = $request->validate([
+                "name" => ['required'],
+                "description" => ['required'],
+                "image" => ['required'],
+                "price" => ['required', 'numeric', 'min:10'],
+                "category_id" => ['required']
+            ]);
+        } catch (ValidationException $e) {
+            return $this->sendError("Validation errors", $e->errors(), 422);
+        }
+
+        $category = Category::find($fields["category_id"]);
+        if (!$category) {
+            return $this->sendError("Category ID " . $fields["category_id"] . " does not exists", [], 404);
+        }
+
+        $plant->update($fields);
+        return $this->sendResponse("Plant updated succefully", $plant, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Plant $plant)
+    {
+        $plant->delete();
+        return $this->sendResponse("Plant deleted succefully", []);
+    }
+}
