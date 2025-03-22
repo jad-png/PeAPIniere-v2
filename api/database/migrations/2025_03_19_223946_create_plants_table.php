3f0b3c7938c4438c03ef4a44891566d7c816edcb
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-22 23:58:48 +0000

    Add slug column to plants table for unique identification

--- api/database/migrations/2025_03_19_223946_create_plants_table.php
+++ api/database/migrations/2025_03_19_223946_create_plants_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('plants', function (Blueprint $table) {
             $table->id();
             $table->string("name")->unique();
+            $table->string("slug")->unique();
             $table->text("description");
             $table->decimal("price", 8, 2);
             $table->string("image", 255);
