7b123e9856bb945c638b651c0635b97fd1ebfd92
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-22 22:45:17 +0000

    Add routes for retrieving plants in an order and canceling an order in api.php

--- api/routes/api.php
+++ api/routes/api.php
@@ -16,3 +16,6 @@ Route::get("/profile", [AuthController::class, 'profile'])->middleware("jwt.auth
 Route::apiResource("plants", PlantController::class);
 Route::apiResource("categories", CategoryController::class);
 Route::apiResource("orders", OrderController::class)->middleware("jwt.auth");
+
+Route::get("orders/{order}/plants", [OrderController::class, "plantsOrder"]);
+Route::get("orders/{order}/cancel", [OrderController::class, "cancel"]);
