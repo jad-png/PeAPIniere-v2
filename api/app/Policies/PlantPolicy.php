c5a05d4552c65529508ec30a077e26138711b5e8
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-23 00:48:01 +0000

    Add PlantPolicy for authorization control on Plant model actions

---
+++ api/app/Policies/PlantPolicy.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Plant;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PlantPolicy
+{
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Plant $plant): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->role_id === 1;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Plant $plant): bool
+    {
+        return $user->role_id === 1;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Plant $plant): bool
+    {
+        return $user->role_id === 1;
+    }
+}
