3849706f90cdc16dfbbe5d2499bb097c05f9dc60
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-24 23:40:23 +0000

    Bind CategoryInterface to CategoryDAO in AppServiceProvider for improved dependency management

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -2,8 +2,13 @@
 
 namespace App\Providers;
 
+use App\DAOs\CategoryDAO;
+use App\DAOs\Interfaces\CategoryInterface;
+use App\Models\Category;
 use App\Models\Plant;
+use App\Policies\CategoryPolicy;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CategoryInterface::class, CategoryDAO::class);
     }
 
     /**
