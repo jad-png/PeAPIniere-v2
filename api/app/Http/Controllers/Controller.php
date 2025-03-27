ada8a5574680e013f23184df210943cc32452c08
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-27 10:45:13 +0000

    Add OpenAPI annotations to Controller for API documentation

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -6,7 +6,13 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Attributes as OA;
 
+#[
+    OA\Info(version: "1.0.0", description: "PeAPIniere api documentation", title: "PeAPIniere"),
+    OA\Server(url: "http://127.0.0.1:8000/api", description: "local server"),
+    OA\SecurityScheme(securityScheme: "bearerAuth", type: "http", name: "Authorization", in: "header", scheme: "bearer"),
+]
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
