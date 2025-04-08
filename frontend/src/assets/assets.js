b97b17451b573eedd5e08fb31a67720e84bca1c2
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-04-08 22:12:46 +0100

    feat: add assets module for managing SVG icons

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,7 @@
+import logo from "./logo.svg";
+import logout from "./logout.svg";
+import user from "./user.svg";
+import cart from "./cart.svg";
+import chevron_down from "./chevron-down.svg";
+
+export const assets = { logo, logout, user, cart, chevron_down };
