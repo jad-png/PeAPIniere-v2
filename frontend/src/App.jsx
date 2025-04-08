082507f7e05f931dc7373ec4a9ea6e069fbf7e9a
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-04-08 22:08:39 +0100

    feat: restructure App component to include Navbar and Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen justify-between">
-      <main className="flex-grow">
+      <Navbar />
+      <main className="flex-grow bg-gray-50">
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 }
