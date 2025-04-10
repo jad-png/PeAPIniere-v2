1cb1d43b2e63001674cf51289c27bb2f845cc5df
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-04-10 22:21:52 +0100

    Add OrdersPage route for managing user orders

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,6 +14,7 @@ import PrivateRoute from "./components/PrivateRoute.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import ProductDetail from "./pages/ProductDetail.jsx";
 import Register from "./pages/auth/Register.jsx";
+import OrdersPage from "./pages/OrdersPage.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,6 +23,7 @@ const router = createBrowserRouter(
         <Route path="" index={true} element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/plants/:slug" element={<ProductDetail />} />
+        <Route path="/cart" element={<OrdersPage />} />
       </Route>
 
       <Route path="/login" element={<Login />} />
